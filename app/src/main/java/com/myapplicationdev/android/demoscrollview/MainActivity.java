63bb13c370b50ddc6fa5576a7a65957f3432e751
AnjanaGeorge <16004097@myrp.edu.sg>
2018-04-16 14:02:33 +0800

    ScrollView

--- app/src/main/java/com/myapplicationdev/android/demoscrollview/MainActivity.java
+++ app/src/main/java/com/myapplicationdev/android/demoscrollview/MainActivity.java
@@ -2,12 +2,18 @@ package com.myapplicationdev.android.demoscrollview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button button;
+    EditText editText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button = (Button) findViewById(R.id.button);
+        editText = (EditText) findViewById(R.id.editText);
     }
 }
